2de6bfdb9e6ebd0c7ab82a0ff5c8736def586bff
OmarMazin <77233657+omarmazin@users.noreply.github.com>
2023-03-02 18:35:25 -0800

    Update data.py

--- data.py
+++ data.py
@@ -1,6 +1,7 @@
 
 my_email = "example@gmail.com"
 my_password = "PWA"
+# Note : GENERATE YOUR PASSWORD FROM >> Google account > App password > Then Create
 file = open("target.txt", "r")
 for x in file:    
     receivers = x
